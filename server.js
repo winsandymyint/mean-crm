f69b21f6dbe3d4e766f761a22b0678fe43fa8b82
yeminhtut <yehtut.it@gmail.com>
2015-10-15 15:47:25 +0800

    heroku proc

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.get('*', function(req, res) {
 
 // START THE SERVER
 // ====================================
-app.listen(8080);
+app.listen(config.port);
 console.log('Magic happens on port ' + config.port);
